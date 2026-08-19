159d532c4a6085f477ddb95f89e6917bafc07fa9
utkarshaglitched <utkarshaavinash@gmail.com>
2026-08-19 18:12:59 +0530

    initialised database

---
+++ db_methods/db_initialise.py
@@ -0,0 +1,29 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session,declarative_base,Mapped,mapped_column
+from dotenv import load_dotenv
+from datetime import datetime
+
+
+load_dotenv()
+
+database_url=os.getenv("DATABASE_URL")
+engine=create_engine(database_url)
+
+def get_session():
+    return Session(engine)
+
+
+Base=declarative_base()
+
+class NotificationQueue(Base):
+    __tablename__ = "notification_queue"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    order_id: Mapped[int] = mapped_column()
+    product: Mapped[str] = mapped_column()
+    notification_type: Mapped[str] = mapped_column()
+    created_at: Mapped[datetime] = mapped_column()
+    status: Mapped[str] = mapped_column()
+
+    
